cef63b102443d3a03d8731a18f814463e049d321
Mike Emery <memery@sofi.org>
2016-05-24 16:11:17 -0600

    switched from coffee to the more godly javascript

--- public/javascripts/hello.js
+++ public/javascripts/hello.js
@@ -1,3 +1,11 @@
 if (window.console) {
   console.log("Welcome to your Play application's JavaScript!");
 }
+
+$(document).ready(function() {
+  $.get('/tasks', function(data) {
+    data.forEach(function(e) {
+      $('#tasks').append($("<li>").text(e.contents));
+    });
+  });
+});
